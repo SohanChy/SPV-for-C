8f9634455bad51a22a758240b499926bb54182c5
Sohan Chowdhury <sifat3d@gmail.com>
2016-01-22 19:31:41 +0600

    Added zero initialization to resize function as well

--- spv.h
+++ spv.h
@@ -7,41 +7,43 @@
 /*
 *****************************************************************************************
                           SPV - Sohans Psuedo Vector for C
-		            Use C++ STL vectors, in C!
+                            Use C++ STL vectors, in C!
                             Author: Sohan Chowdhury
                             Email: Sifat3D@gmail.com
                             Website: www.sohan.cf
 
-	Use C++ STL vectors, in C!
-        Sohans Psuedo Vector for C or SPV is a struct and a collection of functions
-        in C which allows resizing, inserting, deleting, sorting of arrays and much
-	much more.
+    
+    Sohans Psuedo Vector for C or SPV is a struct and a collection of functions
+    in C which allows resizing, inserting, deleting, sorting of arrays and much
+    much more.
 
-        SPV contains almost every feature of C++ STL vectors, but it does not claim
-        to be as efficient or fast. SPV uses the same concept as C++ vectors, by
-	creating new arrays and copying old arrays to it. The memory consumption and
-	runtime can grow pretty large if the size of the array is significant because
-	SPV does not have the optimizations of STL vectors. But for small programs or
-	projects where C is necessary but memory is not that much of a concern and the
-	data size is not extremely large, SPV can be used as a make shift alternative
-	to C++ vector functionality.
+    SPV contains almost every feature of C++ STL vectors, but it does not claim
+    to be as efficient or fast. SPV uses the same concept as C++ vectors, by
+    creating new arrays and copying old arrays to it. The memory consumption and
+    runtime can grow pretty large if the size of the array is significant because
+    SPV does not have the optimizations of STL vectors. But for small programs or
+    projects where C is necessary but memory is not that much of a concern and the
+    data size is not extremely large, SPV can be used as a make shift alternative
+    to C++ vector functionality.
 
         Using SPV is as simple as typing:
-		int size = 1;
-		SPV example;
-		SPVconstruct( size , &example)
-		//Thats it!
-		//Now you can SPV as simply as:
-		example.spv[0] = 5;
+            int size = 1;
+            SPV example;
+            SPVconstruct( size , &example)
+
+            //Thats it!
+            //Now you can SPV as simply as:
+            
+            example.spv[0] = 5;
 
         SPVconstruct() MUST be called before using a SPV object, it is recommended to
         call it right below the declaration.
 
-	The default data type is int, but it can be easily changed by simply editing the spv.h file.
-		//Change %d to the appropriate printf format-specifier for your data type of choice.
-		#define SPVprintf "%d "
-		//Change int to whatever data type you want.
-		typedef int SPVtype;
+        The default data type is int, but it can be easily changed by simply editing the spv.h file.
+        //Change %d to the appropriate printf format-specifier for your data type of choice.
+        #define SPVprintf "%d "
+        //Change int to whatever data type you want.
+        typedef int SPVtype;
 
 *****************************************************************************************
 */
@@ -72,13 +74,16 @@ void SPVconstruct(int size, SPV* this)  //constructor function, zero initializes
         {
             int i, counter = 0;
 
-            for(i = this->first; i<=this->last && counter<5; i++)	//if first 5 elements are 0, assume compiler zero initialized it.
+            for(i = this->first; i<=this->last && counter<5; i++)
             {
                 if(this->spv[i] == 0)
                 {
                     counter++;
                 }
-                this->spv[i] = 0;
+                else
+                {
+                    this->spv[i] = 0;
+                }
             }
         }
 }
@@ -134,6 +139,21 @@ void SPVresize(SPV* this, int change)   //resize array, change can be positive o
     if(newSize > this->size)
         {
             limit = this->size;
+
+            //zero initialization just to be safe
+            int i, counter = 0;
+
+            for(i = this->last; i<= (newSize-1) && counter<5; i++)
+            {
+                if(tmpArr[i] == 0)
+                {
+                    counter++;
+                }
+                else
+                {
+                    tmpArr[i] = 0;
+                }
+            }
         }
     else
         {
